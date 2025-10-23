ea806da721a1697a3558a5bac7dcf289c880b42b
pepu <pepshove@gmail.com>
2025-10-23 18:15:23 +0200

    iac-files

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -63,7 +63,7 @@ variable "node_groups" {
       desired_size   = 2
       max_size       = 2
       min_size       = 1
-      instance_types = ["t3.small"]
+      instance_types = ["t3.medium"]
     }
   }
 }
